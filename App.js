2e4070c8183a93e0bc0905eeaae2236d7230300a
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-08-16 19:40:52 +0100

    Working route generation on initial render

--- App.js
+++ App.js
@@ -1,25 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
-import { lineString } from '@turf/helpers';
 
 const MAPBOX_API_KEY = 'pk.eyJ1IjoibmFzc2ltY2hlbm91ZiIsImEiOiJja2R1NjE2amMzYnl4MzByb3c5YmxlMGY5In0.cBj3YeAh0UMxinxOfhDLIw';
 
 MapboxGL.setAccessToken(MAPBOX_API_KEY);
 MapboxGL.setConnected(true);
 
-const originCoordinates = [-1.55459, 55.0198];
-const destinationCoordinates = [-1.5491, 53.8008];
+const coordsLst = [ [-1.55459, 55.0198], [-1.5491, 53.8008], [-0.118092, 51.509865] ];
+const generateCoordsList = () => {
+  const coordsURLLst = [];
+  for (let i = 0; i < coordsLst.length; i++) {
+    coordsURLLst.push(coordsLst[i].join())
+  };
+  const coordsURLString = coordsURLLst.join(';');
+  return coordsURLString;
+};
 
-const line = lineString([originCoordinates, destinationCoordinates]);
-
-
+const routeCoordsString = generateCoordsList();
 
 const App = () => {
-  const [routeLineString, setRouteLineString] = useState({});
+  const [routeLineString, setRouteLineString] = useState({ "type": "LineString", "coordinates": [] });
+  
 
   const fetchRouteCoords = async() => {
-    const response = await fetch(`https://api.mapbox.com/directions/v5/mapbox/walking/-1.55459%2C55.0198%3B-1.5491%2C53.8008?alternatives=false&geometries=geojson&steps=true&access_token=pk.eyJ1IjoibmFzc2ltY2hlbm91ZiIsImEiOiJja2R1NjE2amMzYnl4MzByb3c5YmxlMGY5In0.cBj3YeAh0UMxinxOfhDLIw`);
+    const response = await fetch(`https://api.mapbox.com/directions/v5/mapbox/walking/${routeCoordsString}?alternatives=false&geometries=geojson&steps=true&access_token=${MAPBOX_API_KEY}`);
     const data = await response.json();
     setRouteLineString(data.routes[0].geometry);
   };
@@ -27,6 +32,8 @@ const App = () => {
   useEffect(() => {
     fetchRouteCoords();
   }, [])
+
+  console.log(routeLineString)
 
   return (
     <View style = {styles.page}>
