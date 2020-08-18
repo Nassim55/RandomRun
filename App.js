085d2cf141666b4b1798ce6a1b1d0d350a6b78b1
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-08-18 19:35:34 +0100

    Optimising algorithm on the back-end now works relitivly accurate

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
+import { lineString } from '@turf/helpers';
 
 const MAPBOX_API_KEY = 'pk.eyJ1IjoibmFzc2ltY2hlbm91ZiIsImEiOiJja2R1NjE2amMzYnl4MzByb3c5YmxlMGY5In0.cBj3YeAh0UMxinxOfhDLIw';
 
@@ -9,18 +10,19 @@ MapboxGL.setConnected(true);
 
 const App = () => {
   const [randomRouteCoords, setRandomRouteCoords] = useState({'coordinates': []});
+  const [randomRouteCoordsLineString, setRandomRouteCoordsLineString] = useState({ "type": "LineString", "coordinates": [] });
   const [routeLineString, setRouteLineString] = useState({ "type": "LineString", "coordinates": [] });
 
   const originLongitude = -1.55459;
   const originLatitude = 55.0198;
-  const routeDistanceMeters = 2000;
+  const routeDistanceMeters = 3000;
   
   const fetchRandomCoords = async () => {
     try{
       const response = await fetch(`http://127.0.0.1:5000/route?longitude=${originLongitude}&latitude=${originLatitude}&routeDistance=${routeDistanceMeters}`);
       const data = await response.json();
       setRandomRouteCoords(data.coordinates);
-      //console.log(randomRouteCoords);
+      setRandomRouteCoordsLineString(lineString(data.coordinates))
     } catch (err) {
       if (console) {
         console.error(err);
@@ -38,22 +40,35 @@ const App = () => {
   };
 
   const routeCoordsString = generateCoordsString(randomRouteCoords);
+  
 
   const fetchRouteCoords = async () => {
     try {
-      const response = await fetch(`https://api.mapbox.com/directions/v5/mapbox/walking/${routeCoordsString}?alternatives=false&geometries=geojson&steps=true&&continue_straight=true&access_token=${MAPBOX_API_KEY}`);
+      const response = await fetch(`https://api.mapbox.com/directions/v5/mapbox/walking/${routeCoordsString}?alternatives=false&geometries=geojson&steps=true&continue_straight=true&access_token=${MAPBOX_API_KEY}`);
       const data = await response.json();
       setRouteLineString(data.routes[0].geometry);
       console.log(data.routes[0].distance)
       if (data.routes[0].distance > routeDistanceMeters) {
-        for (let i = 0; i < 10; i++) {
-          if (data.routes[0].distance > routeDistanceMeters) {
-            console.log('greater') 
-          } else {
-            break;
+        try {
+          console.log('optimising')
+          const scaleResponse = await fetch(`http://127.0.0.1:5000/optimise`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              "mapboxRouteGeometry": data.routes[0].geometry.coordinates
+            })
+          });
+          const scaledData = await scaleResponse.json();
+          console.log(scaledData.distanceMeters);
+          setRouteLineString({ 'type': 'LineString', 'coordinates': scaledData.coordinates });
+        } catch (err) {
+          if (console) {
+            console.error(err);
           }
-        }
-      } 
+        };
+      }
     } catch (err) {
       if (console) {
         console.error(err);
@@ -72,6 +87,9 @@ const App = () => {
         <MapboxGL.MapView style = {styles.map} >
           <MapboxGL.ShapeSource id="origin" shape={routeLineString}>
             <MapboxGL.LineLayer id="routeFill" style={layerStyles.route} />
+          </MapboxGL.ShapeSource>
+          <MapboxGL.ShapeSource id="line" shape={randomRouteCoordsLineString}>
+            <MapboxGL.LineLayer id="randomLine" style={layerStyles.randomRouteLine} />
           </MapboxGL.ShapeSource>
           <MapboxGL.PointAnnotation id="origin-point" coordinate={[-1.55459, 55.0198]} />
         </MapboxGL.MapView>
@@ -101,9 +119,15 @@ const layerStyles = {
   route: {
     lineColor: 'red',
     lineCap: MapboxGL.LineJoin.Round,
-    lineWidth: 3,
+    lineWidth: 2,
     lineOpacity: 0.84,
-  }
+  },
+  randomRouteLine: {
+    lineColor: 'blue',
+    lineCap: MapboxGL.LineJoin.Round,
+    lineWidth: 2,
+    lineOpacity: 0.84,
+  },
 };
 
 
