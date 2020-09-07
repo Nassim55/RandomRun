543d904049db859d0d05303b99a9713bfd25d4c4
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-09-07 07:54:42 +0100

    designed login page

--- app/components/LoginPageView.js
+++ app/components/LoginPageView.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, Pressable } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { useHistory } from "react-router-native";
+import { TextInput, Button } from 'react-native-paper'
 
 // Custom functions:
 import userAuthentication from '../authentication/userAuthentication';
@@ -20,22 +21,60 @@ const LoginPageView = () => {
 
     return (
         <View style = {styles.pageContent}>
-            <View style={styles.formContainer}>
-                <Text>Member Sign In</Text>
-                <TextInput
-                style = {styles.userInputForm}
-                placeholder = 'Email Address...'
-                onChangeText={username => setUsername(username)}
-                />
-                <TextInput
-                style = {styles.userInputForm}
-                placeholder = 'Password...'
-                secureTextEntry={true}
-                onChangeText={password => setPassword(password)}
-                />
-                <Pressable onPress={() => userAuthentication(username, password, dispatch, history)}>
-                    <Text>Login</Text>
-                </Pressable>
+            <View style={styles.welcomeContainer}>
+                <Text style={styles.titleText}>Random Run</Text>
+            </View>
+            <View style={styles.cardsContainer}>
+                <View style={styles.card}>
+                    <Text style={styles.welcomeTextTop}>Already have an account?</Text>
+                    <Text style={styles.welcomeTextBottom}>Pick up where you left off</Text>
+                    <TextInput
+                    style={styles.inputForm}
+                    label="Email"
+                    value={username}
+                    onChangeText={username => setUsername(username)}
+                    mode={'outlined'}
+                    />
+                    <TextInput
+                    style={styles.inputForm}
+                    label="Password"
+                    value={password}
+                    secureTextEntry={true}
+                    onChangeText={password => setPassword(password)}
+                    mode={'outlined'}
+                    />
+                    <Button
+                    style={styles.loginButton}
+                    uppercase={false}
+                    icon='login'
+                    mode="contained"
+                    onPress={() => userAuthentication(username, password, dispatch, history)}
+                    >
+                        Login
+                    </Button>
+                </View>
+                <View style={styles.card}>
+                    <Text style={styles.welcomeTextTop}>New to Random Run?</Text>
+                    <Text style={styles.welcomeTextBottom}>Start your adventure now</Text>
+                    <Button
+                    style={styles.loginButton}
+                    uppercase={false}
+                    icon='sign-direction'
+                    mode="outlined"
+                    onPress={() => userAuthentication(username, password, dispatch, history)}
+                    >
+                        Sign Up
+                    </Button>
+                    <Button
+                    style={styles.loginButton}
+                    uppercase={false}
+                    icon='google'
+                    mode="outlined"
+                    onPress={() => userAuthentication(username, password, dispatch, history)}
+                    >
+                        Sign Up With Google
+                    </Button>
+                </View>
             </View>
         </View>
     );
@@ -43,38 +82,60 @@ const LoginPageView = () => {
 
 const styles = StyleSheet.create({
     pageContent: {
+        backgroundColor: 'white',
         position: 'relative',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         width: '100%',
         height: '100%',
+        
     },
-    formContainer: {
-        position: 'relative',
+    titleText: {
+        fontFamily: 'Raleway-Regular',
+        fontSize: 44,
+    },
+    welcomeContainer: {
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'space-evenly',
+        justifyContent: 'center',
         alignItems: 'center',
-        width: '90%',
-    },
-    userInputForm: {
-        position: 'relative',
+        height: '20%',
         width: '100%',
-        backgroundColor: 'white',
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        borderRadius: 10,
-        opacity: 0.8,
-        margin: 5
     },
+    cardsContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        height: '80%',
+        width: '100%',
+    },
+    card: {
+        borderWidth: 2.5,
+        display: 'flex',
+        flexDirection: 'column',
+        width: '85%',
+        borderStyle: 'dashed',
+        borderRadius: 20,
+        borderColor: '#ccc',
+        padding: 20,
+        marginBottom: 15
+    },
+    welcomeTextTop: {
+        fontFamily: 'Raleway-Regular',
+        fontSize: 24,
+    },
+    welcomeTextBottom: {
+        fontFamily: 'Raleway-Light',
+        fontSize: 16,
+    },
+    inputForm: {
+        marginTop: 10,
+    },
+    loginButton: {
+        marginTop: 20
+    }
   });
 
 export default LoginPageView;
