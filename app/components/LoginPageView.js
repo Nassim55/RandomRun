571285d0ef22cbb8a4c453b66a9ad2330cd7b844
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-09-07 10:09:55 +0100

    Working on animation for sign up on login page

--- app/components/LoginPageView.js
+++ app/components/LoginPageView.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, Text, Pressable } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { useHistory } from "react-router-native";
 import { TextInput, Button } from 'react-native-paper'
+import { useSpring, animated } from 'react-spring/native';
 
 // Custom functions:
 import userAuthentication from '../authentication/userAuthentication';
@@ -19,23 +20,49 @@ const LoginPageView = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+
+
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const { translateX, opacity, height1, height2, borderWidth } = useSpring({
+        opacity: isFlipped ? 1 : 0,
+        height1: isFlipped ? '0%' : '50%',
+        height2: isFlipped ? '90%' : '40%',
+        borderWidth: isFlipped ? 0 : 2.5,
+        translateX: isFlipped ? 750 : 0,
+        config: { mass: 12, tension: 500, friction: 80 }
+    });
+
+    console.log(isFlipped)
+
+    const AnimatedView = animated(View);
+    const AnimatedText = animated(Text);
+    const AnimatedTextInput = animated(TextInput);
+    const AnimatedButton = animated(Button);
+
     return (
         <View style = {styles.pageContent}>
             <View style={styles.welcomeContainer}>
                 <Text style={styles.titleText}>Random Run</Text>
             </View>
             <View style={styles.cardsContainer}>
-                <View style={styles.card}>
-                    <Text style={styles.welcomeTextTop}>Already have an account?</Text>
-                    <Text style={styles.welcomeTextBottom}>Pick up where you left off</Text>
-                    <TextInput
+                
+                <AnimatedView 
+                style={[styles.cardLogin, {
+                    height: height1,
+                    transform: [{ translateX: translateX }]
+                }]}
+                >
+                    <AnimatedText style={styles.welcomeTextTop}>Already have an account?</AnimatedText>
+                    <AnimatedText style={styles.welcomeTextBottom}>Pick up where you left off</AnimatedText>
+                    <AnimatedTextInput
                     style={styles.inputForm}
                     label="Email"
                     value={username}
                     onChangeText={username => setUsername(username)}
                     mode={'outlined'}
                     />
-                    <TextInput
+                    <AnimatedTextInput
                     style={styles.inputForm}
                     label="Password"
                     value={password}
@@ -43,7 +70,7 @@ const LoginPageView = () => {
                     onChangeText={password => setPassword(password)}
                     mode={'outlined'}
                     />
-                    <Button
+                    <AnimatedButton
                     style={styles.loginButton}
                     uppercase={false}
                     icon='login'
@@ -51,9 +78,9 @@ const LoginPageView = () => {
                     onPress={() => userAuthentication(username, password, dispatch, history)}
                     >
                         Login
-                    </Button>
-                </View>
-                <View style={styles.card}>
+                    </AnimatedButton>
+                </AnimatedView>
+                <AnimatedView style={[styles.cardSignUp, {height: height2}]}>
                     <Text style={styles.welcomeTextTop}>New to Random Run?</Text>
                     <Text style={styles.welcomeTextBottom}>Start your adventure now</Text>
                     <Button
@@ -61,7 +88,7 @@ const LoginPageView = () => {
                     uppercase={false}
                     icon='sign-direction'
                     mode="outlined"
-                    onPress={() => userAuthentication(username, password, dispatch, history)}
+                    onPress={() => setIsFlipped(!isFlipped)}
                     >
                         Sign Up
                     </Button>
@@ -69,12 +96,11 @@ const LoginPageView = () => {
                     style={styles.loginButton}
                     uppercase={false}
                     icon='google'
-                    mode="outlined"
-                    onPress={() => userAuthentication(username, password, dispatch, history)}
+                    mode='outlined'
                     >
                         Sign Up With Google
                     </Button>
-                </View>
+                </AnimatedView>
             </View>
         </View>
     );
@@ -110,8 +136,10 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         height: '80%',
         width: '100%',
+        overflow: 'hidden'
     },
-    card: {
+    cardLogin: {
+        position: 'absolute',
         borderWidth: 2.5,
         display: 'flex',
         flexDirection: 'column',
@@ -120,7 +148,20 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         borderColor: '#ccc',
         padding: 20,
-        marginBottom: 15
+        marginBottom: 15,
+    },
+    cardSignUp: {
+        position: 'absolute',
+        bottom: 0,
+        borderWidth: 2.5,
+        display: 'flex',
+        flexDirection: 'column',
+        width: '85%',
+        borderStyle: 'dashed',
+        borderRadius: 20,
+        borderColor: '#ccc',
+        padding: 20,
+        marginBottom: 35,
     },
     welcomeTextTop: {
         fontFamily: 'Raleway-Regular',
@@ -139,3 +180,8 @@ const styles = StyleSheet.create({
   });
 
 export default LoginPageView;
+
+
+/*
+
+*/
